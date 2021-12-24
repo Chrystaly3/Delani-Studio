9ee83a2dc5e571d634241dd6bdd2494276b99e5a
Chrystaly3 <josephine.nzioka@student.moringaschool.com>
2021-12-24 17:47:14 +0300

    add service icons js file

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-$("#design-icon").click(function(){
-$("")
+$("#image-icon").click(function(){
+$(".paragraph-1").show();
+$("#image-icon").hide();
 
 
+});
+$(".paragraph-1").click(function(){
+$("#image-icon").show();
+$(".paragraph-1").hide();
+});
+});
+
+$(document).ready(function(){
+$("#development-image").click(function(){
+$(".paragraph-3").show();
+$("#development-image").hide();
 })
+$(".paragraph-3").click(function(){
+$("#development-image").show();
+$(".paragraph-3").hide();
+});
+});
 
-
-})
+$
